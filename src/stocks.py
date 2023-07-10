383e72e27347f81de525bae5ff6590bdf6a26b87
getjake <bozi.chinese@gmail.com>
2023-07-10 07:38:57 -0700

    增加基本信息

---
+++ src/stocks.py
@@ -0,0 +1,17 @@
+import logging
+from typing import List
+from dotenv import load_dotenv
+import httpx
+import yfinance as yf
+import pandas as pd
+
+
+
+class StockUtils:
+    def __init__(self) -> None:
+        pass
+
+
+    def get_all_tickers(self) -> List:
+
+
