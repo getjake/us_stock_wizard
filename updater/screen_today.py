9f88f0a08c250230a73a8364a849dc3d941765c1
getjake <bozi.chinese@gmail.com>
2023-07-15 17:02:23 -0700

    minor update

--- updater/screen_today.py
+++ updater/screen_today.py
@@ -1,8 +1,25 @@
 import asyncio
+import pandas as pd
+import logging
 from us_stock_wizard.screener.daily_screener import DailyScreener
+from us_stock_wizard.database.db_utils import StockDbUtils, DbTable
+
+
+async def check_trading_day() -> bool:
+    cal = await StockDbUtils.read(DbTable.TRADING_CALENDAR, output="df")
+    cal["date"] = pd.to_datetime(cal["date"]).dt.date
+    # check today in cal["date"]
+    today = pd.Timestamp.today().date()
+    is_today_trading = today in cal["date"].tolist()
+    if not is_today_trading:
+        logging.warning("Today is not a trading day, skip")
+    return is_today_trading
 
 
 async def main():
+    # is_today_trading = await check_trading_day()
+    # if not is_today_trading:
+    #     return
     screener = DailyScreener()
     await screener.initialize()
     await screener.screen_all()
