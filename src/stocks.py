48e3204ea7be6c61e1d9bc2103bf59417159fca8
getjake <bozi.chinese@gmail.com>
2023-08-30 11:10:18 -0700

    remove blank checks ind.

--- src/stocks.py
+++ src/stocks.py
@@ -98,6 +98,7 @@ class StockTickers:
         _data = _data[~_data["name"].str.contains("Acquisition")]
         _data = _data[~_data["name"].str.contains("acquisition")]
         _data = _data[~_data["ticker"].str.contains("^")]
+        _data = _data[~_data["industry"].str.contains("Blank Checks")]
 
         _data = _data[columns.values()]
         all_results = _data.to_dict(orient="records")
