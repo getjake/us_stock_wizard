8f7d33c7c9237364117539b34ff424ec5f60970f
getjake <bozi.chinese@gmail.com>
2023-11-11 20:14:13 -0800

    update fundamental logic.

--- updater/full_fundamentals.py
+++ updater/full_fundamentals.py
@@ -6,12 +6,15 @@ Run this script every 7 days.
 import asyncio
 import logging
 from us_stock_wizard.src.fundamentals import Fundamentals
+from us_stock_wizard.screener.good_fundamentals_screener import GoodFundamentalsScreener
 
 
 async def main():
     fundamentals = Fundamentals()
     await fundamentals.handle_earning_call_data()
     await fundamentals.handle_all_is_data(filter="all")
+    gfs = GoodFundamentalsScreener()
+    await gfs.run()
     logging.info("Done Full Earning Call and Fundamental")
 
 
